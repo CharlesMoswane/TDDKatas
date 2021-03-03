e561c0031e5c8dc16b64cd837e58b3b6d652ba34
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2021-03-03 14:59:32 +0200

    added class StringCalculatorTest

--- src/com/umuzi/Main.java
+++ src/com/umuzi/Main.java
@@ -1,9 +1,16 @@
 package com.umuzi;
 
+import org.testng.annotations.Test;
+import com.umuzi.StringCalculatorTest;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello world");
     }
+}
+
+class StringCalculatorTest{
+
 }
