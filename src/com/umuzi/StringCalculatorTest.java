994f30b011f204d65f7c8ccde3b370b51f4a0a48
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2021-03-05 13:16:24 +0200

    added the meat to make the tests pass

---
+++ src/com/umuzi/StringCalculatorTest.java
@@ -0,0 +1,25 @@
+package com.umuzi;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class StringCalculatorTest{
+
+    @Test(expectedExceptions = RuntimeException.class)
+    public final void whenMoreThan2NumbersAreUsedThenExceptionIsThrown(){
+        StringCalculator.add("1,2,3");
+    }
+
+    @Test
+    public final void when2NumbersAreUsedThenNoExceptionIsThrown() {
+        StringCalculator.add("1,2");
+        Assert.assertTrue(true);
+    }
+
+    @Test(expectedExceptions = RuntimeException.class)
+    public final void whenNonNumberIsUsedThenExceptionIsThrown() {
+        StringCalculator.add("1,X");
+    }
+}
+
+
