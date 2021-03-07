4687152a11dbcaaf3c89b6d13efcc16cbf17bc94
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2021-03-07 23:20:27 +0200

    added the second failing test

--- src/com/umuzi/StringCalculatorTest.java
+++ src/com/umuzi/StringCalculatorTest.java
@@ -55,6 +55,18 @@ public class StringCalculatorTest{
     public final void whenNegativeNumberIsUsedThenRuntimeExceptionIsThrown(){
         StringCalculator.add("3,-6,15,18,46,33");
     }
+
+    @Test
+    public final void whenNegativeNumbersAreUsedThenRuntimeExceptionIsThrown(){
+        RuntimeException exception = null;
+        try {
+            StringCalculator.add("3,-6,15,18,46,33");
+        }catch (RuntimeException e){
+            exception = e;
+        }
+        Assert.assertNotNull(exception);
+        Assert.assertEquals("Negatives not allowed: [-6, -18]", exception.getMessage());
+    }
 }
 
 
